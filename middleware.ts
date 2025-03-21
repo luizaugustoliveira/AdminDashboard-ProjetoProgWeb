d4802b8c351b07a12551d6c0d1bc8d09157aeff5
Luiz Augusto Oliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2025-03-20 21:27:45 -0300

    clerk fixes

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
+  publicRoutes: ["/", "/sign-in", "/sign-up"], // Permite acesso sem login
 });
 
 export const config = {
